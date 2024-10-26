1cf5f5c1bccbcc918b1dfbe378f2514639c5caf6
Geoff Von Allmen <geoff@ibleducation.com>
2024-10-26 00:51:14 -0700

    refactor: Add --version argument

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+var version = "0.0.1"
+
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "--version" {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
 	p := tea.NewProgram(NewBoard())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Error starting program: %v", err)
