2410ad0288e035ae5094294e2e34e90b61a975f9
Geoff Von Allmen <geoff@ibleducation.com>
2024-10-27 00:16:02 -0700

    test: Combine P2/P2 tests

--- update_test.go
+++ update_test.go
@@ -6,25 +6,35 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var rightMovesP1 = []struct {
-	name        string
-	numMoves    uint8
-	expectedPit uint8
-}{
-	{"Right1", 1, 1},
-	{"Right2", 2, 2},
-	{"Right3", 3, 3},
-	{"Right4", 4, 4},
-	{"Right5", 5, 5},
-	{"Right6", 6, 0},
-}
-
-func TestMoveRight_P1(t *testing.T) {
+func TestMoveRight(t *testing.T) {
+	var moves = []struct {
+		name        string
+		numMoves    uint8
+		expectedPit uint8
+		player      Player
+	}{
+		{"Right1P1", 1, 1, P1},
+		{"Right2P1", 2, 2, P1},
+		{"Right3P1", 3, 3, P1},
+		{"Right4P1", 4, 4, P1},
+		{"Right5P1", 5, 5, P1},
+		{"Right6P2", 6, 0, P1},
+		{"Right1P2", 1, 11, P2},
+		{"Right2P2", 2, 10, P2},
+		{"Right3P2", 3, 9, P2},
+		{"Right4P2", 4, 8, P2},
+		{"Right5P2", 5, 7, P2},
+		{"Right6P2", 6, 12, P2},
+	}
 	assert := assert.New(t)
 
-	for _, move := range rightMovesP1 {
+	for _, move := range moves {
 		t.Run(move.name, func(t *testing.T) {
 			state := NewState()
+			if move.player == P2 {
+				state.SwitchPlayer()
+			}
+
 			for range move.numMoves {
 				state.MoveRight()
 			}
@@ -33,60 +43,30 @@ func TestMoveRight_P1(t *testing.T) {
 	}
 }
 
-var rightMovesP2 = []struct {
-	name        string
-	numMoves    uint8
-	expectedPit uint8
-}{
-	{"Right1", 1, 11},
-	{"Right2", 2, 10},
-	{"Right3", 3, 9},
-	{"Right4", 4, 8},
-	{"Right5", 5, 7},
-	{"Right6", 6, 12},
-}
-
-func TestMoveRight_P2(t *testing.T) {
-	assert := assert.New(t)
-
-	for _, move := range rightMovesP2 {
-		t.Run(move.name, func(t *testing.T) {
-			state := NewState()
-			state.SwitchPlayer()
-			state.selectedPit = uint8(12)
-			for range move.numMoves {
-				state.MoveRight()
-			}
-			assert.Equal(int(state.selectedPit), int(move.expectedPit))
-		})
-	}
-}
-
-var leftMoves = []struct {
-	name        string
-	numMoves    uint8
-	expectedPit uint8
-	player      Player
-}{
-
-	{"Left1P1", 1, 5, P1},
-	{"Left2P1", 2, 4, P1},
-	{"Left3P1", 3, 3, P1},
-	{"Left4P1", 4, 2, P1},
-	{"Left5P1", 5, 1, P1},
-	{"Left6P1", 6, 0, P1},
-	{"Left2P1", 1, 7, P2},
-	{"Left2P2", 2, 8, P2},
-	{"Left3P2", 3, 9, P2},
-	{"Left4P2", 4, 10, P2},
-	{"Left5P2", 5, 11, P2},
-	{"Left6P2", 6, 12, P2},
-}
-
 func TestMoveLeft(t *testing.T) {
+	var moves = []struct {
+		name        string
+		numMoves    uint8
+		expectedPit uint8
+		player      Player
+	}{
+
+		{"Left1P1", 1, 5, P1},
+		{"Left2P1", 2, 4, P1},
+		{"Left3P1", 3, 3, P1},
+		{"Left4P1", 4, 2, P1},
+		{"Left5P1", 5, 1, P1},
+		{"Left6P1", 6, 0, P1},
+		{"Left2P1", 1, 7, P2},
+		{"Left2P2", 2, 8, P2},
+		{"Left3P2", 3, 9, P2},
+		{"Left4P2", 4, 10, P2},
+		{"Left5P2", 5, 11, P2},
+		{"Left6P2", 6, 12, P2},
+	}
 	assert := assert.New(t)
 
-	for _, move := range leftMoves {
+	for _, move := range moves {
 		t.Run(move.name, func(t *testing.T) {
 			state := NewState()
 			if move.player == P2 {
