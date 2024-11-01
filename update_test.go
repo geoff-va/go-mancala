f3eaa93f467b3b8b5daa5caedbde1204f4bfac2d
Geoff Von Allmen <geoff@ibleducation.com>
2024-11-01 11:53:55 -0700

    test: Fix update_test errors so it runs again

--- update_test.go
+++ update_test.go
@@ -6,7 +6,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMoveRight(t *testing.T) {
+// Testing Moving right and wrapping around
+func TestHandleMoveRight(t *testing.T) {
 	var cases = []struct {
 		name        string
 		numMoves    uint8
@@ -19,6 +20,7 @@ func TestMoveRight(t *testing.T) {
 		{"Right4P1", 4, 5, P1},
 		{"Right5P1", 5, 6, P1},
 		{"Right6P2", 6, 1, P1},
+		// P2 is moving right which is decrementing
 		{"Right1P2", 1, 12, P2},
 		{"Right2P2", 2, 11, P2},
 		{"Right3P2", 3, 10, P2},
@@ -57,6 +59,7 @@ func TestMoveLeft(t *testing.T) {
 		{"Left4P1", 4, 3, P1},
 		{"Left5P1", 5, 2, P1},
 		{"Left6P1", 6, 1, P1},
+		// P2 is moving right which is incrementing
 		{"Left2P1", 1, 8, P2},
 		{"Left2P2", 2, 9, P2},
 		{"Left3P2", 3, 10, P2},
@@ -102,47 +105,15 @@ func TestSelectPit(t *testing.T) {
 	assert.Equal(uint8(4), state.inHand, "inHand")
 	assert.Equal(state.selectedPit, state.lastPlacedPit, "lastPlacedPit")
 	assert.Equal(MovingFromHandToPit, nextState, "state")
-	assert.Equal(state.selectedPit, state.lastSelectedPit[state.currentPlayer], "lastSelectedPit")
 
 }
-
-func TestGetOppositePit(t *testing.T) {
-	var cases = []struct {
-		name     string
-		pit      uint8
-		expected uint8
-	}{
-		{"Opposite1", 1, 13},
-		{"Opposite2", 2, 12},
-		{"Opposite3", 3, 11},
-		{"Opposite4", 4, 10},
-		{"Opposite5", 5, 9},
-		{"Opposite6", 6, 8},
-		{"Opposite8", 8, 6},
-		{"Opposite9", 9, 5},
-		{"Opposite10", 10, 4},
-		{"Opposite11", 11, 3},
-		{"Opposite12", 12, 2},
-		{"Opposite13", 13, 1},
-	}
-	assert := assert.New(t)
-
-	for _, tcase := range cases {
-		t.Run(tcase.name, func(t *testing.T) {
-			assert.Equal(tcase.expected, GetOppositePit(tcase.pit))
-		})
-	}
-
-}
-
 func TestMovingFromHandToPit_HandNotEmpty(t *testing.T) {
 	state := NewState()
 
 	state.state = state.HandleSelectPit()
-	nextState, isDone := state.HandleMoveFromHandToPit()
+	nextState := state.HandleMoveFromHandToPit()
 
 	assert.Equal(t, MovingFromHandToPit, nextState, "next state")
-	assert.Equal(t, false, isDone, "isDone")
 
 }
 
@@ -150,13 +121,12 @@ func TestMovingFromHandToPit_EmptyHandTurnOver(t *testing.T) {
 	state := NewState()
 
 	state.state = state.HandleSelectPit()
-	state.state, _ = state.HandleMoveFromHandToPit()
-	state.state, _ = state.HandleMoveFromHandToPit()
-	state.state, _ = state.HandleMoveFromHandToPit()
-	state.state, _ = state.HandleMoveFromHandToPit()
-	nextState, isDone := state.HandleMoveFromHandToPit()
+	state.state = state.HandleMoveFromHandToPit()
+	state.state = state.HandleMoveFromHandToPit()
+	state.state = state.HandleMoveFromHandToPit()
+	state.state = state.HandleMoveFromHandToPit()
+	nextState := state.HandleMoveFromHandToPit()
 
-	assert.Equal(t, MovingFromHandToPit, nextState, "next state")
-	assert.Equal(t, true, isDone, "isDone")
+	assert.Equal(t, SwitchPlayer, nextState, "next state")
 
 }
